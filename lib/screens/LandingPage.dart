a885595ca16720d769514cfbdd740ff7f5db5eb8
md_faz <mfazii4563@gmail.com>
2023-02-03 22:58:58 +0530

    work started

---
+++ lib/screens/LandingPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 365.w,
+      height: 667.h,
+      child: const Center(
+        child: Text("Work Started"),
+      ),
+    );
+  }
+}
