5362aa24d93c9bec53455239dc51bfcff67ae70f
faz4563 <mfazii4563@gmail.com>
2023-04-03 13:09:56 +0530

    cleaning

--- android/app/src/main/kotlin/com/example/red_drop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.red_drop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/images.dart
+++
@@ -1,5 +0,0 @@
-//Images ---------------->
-const String bannerImage1 = "assets/images/pic1.jpg";
-const String bannerImage2 = "assets/images/pic2.jpg";
-const String bannerImage3 = "assets/images/pic3.jpg";
-const String loginImg = "assets/images/login.jpg";
